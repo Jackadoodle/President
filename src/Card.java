f8176d5593a953b124f19811f3a38cd33f81fb8b
Jackadoodle <51856751+Jackadoodle@users.noreply.github.com>
2019-06-22 10:37:59 -0400

    import everything and create the cards

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+
+public class Card {
+	public Card(int x, char c) {
+		// TODO Auto-generated constructor stub
+		value = x;
+		suit = c;
+	}
+	int value;
+	char suit;
+	
+	
+	
+}
--- src/PresidentCardGameOriginalNoCopies.java
+++ src/PresidentCardGameOriginalNoCopies.java
@@ -1,9 +1,31 @@
-
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import java.applet.Applet;
 public class PresidentCardGameOriginalNoCopies {
-
+	
+	public Card[] generateDeck() {
+		Card[]deck = new Card [52];
+		char[]suits= {'s','c','h','d'};
+		for(int i=0;i<4;i++) {
+			for(int x=1;x<14;x++) {
+				Card c = new Card(x,suits[i]);
+				deck[i*13+(x-1)] = c;
+			}
+		}
+		
+		return deck;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
